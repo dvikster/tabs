e65665132a5ba132ae08e675ffd734adb87b8de8
dvikster <dvikster@gmail.com>
2017-05-05 09:27:27 +0300

    fixed js click

--- js.js
+++ js.js
@@ -2,25 +2,23 @@
 var tablinks = document.getElementsByClassName('tab-h');
 var tabcontent = document.getElementsByClassName('tab-b');
 
-tablinks[0].classList.add('active');
-tabcontent[0].style.display = "block";
-
 for (var i = 0; i < tablinks.length; i++) {
     tablinks[i].onclick = tabChange;
-
 }
 
 function tabChange() {
-
     for (var a = 0; a < tabcontent.length; a++) {
         tabcontent[a].style.display = "none";
     }
     var dataTab= this.getAttribute('data-tab');
     document.querySelector('.tab-b[data-tab="' + dataTab + '"]').style.display = 'block';
-
-
     for (var i = 0; i < tablinks.length; i++) {
         tablinks[i].classList.remove('active');
     }
     this.classList.add('active');
 }
+
+tablinks[0].click();
+
+// tablinks[0].classList.add('active');
+// tabcontent[0].style.display = "block";
